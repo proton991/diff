cc2980fe291993d12f0636d5ed590436cfa6ba59
proton991 <xinyucui99@gmail.com>
2021-06-09 20:07:03 +0800

    first

---
+++ a.java
@@ -0,0 +1,20 @@
+if (log.isDebugEnbabled()) {
+	log.debug(sm.getString("deltaManager.receiveMessage.delta", getName(), msg.getSessionID()));
+}
+Class<?> inf = type.getInterfaces();
+Method mp = null;
+for (Class<?> aClass : inf) {
+
+}
+
+if (!deploymentDescriptor.exists()) {
+	throw new BuildException("Deployment descriptor: " + deploymentDescriptor + " does not exist.");
+}
+
+i = (b+a) / 2;
+Element currentTest;
+if (test != null) {
+	currentTest = testElements.get(creatDescription(test));
+} else {
+	currentTest = rootElement;
+}
